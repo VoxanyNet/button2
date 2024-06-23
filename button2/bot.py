e08bdc788edf2f7671875b4a961df14b857d6bd9
VoxanyNet <contact@voxany.net>
2024-06-23 17:40:04 -0400

    counter since last fail

--- button2/bot.py
+++ button2/bot.py
@@ -17,7 +17,8 @@ class ButtonBot2(discord.Bot):
 
     DEFAULT_BUTTON_DATA = {
         "target_time": 1000000000000000,
-        "high_scores": []
+        "high_scores": [],
+        "last_fail": time.time()
     }
 
     def __init__(self, button_data_directory, description=None, *args, **options):
@@ -39,6 +40,7 @@ class ButtonBot2(discord.Bot):
 
         self.target_time: int = data["target_time"]
         self.high_scores: Dict[HighScore] = data["high_scores"]
+        self.last_fail = data["last_fail"]
 
         self.add_application_command(commands.press)
         self.add_application_command(commands.highscores)
@@ -88,27 +90,55 @@ class ButtonBot2(discord.Bot):
 
     async def on_ready(self):
         self.check_if_expired.start()
+        self.update_status.start()
+
+    @tasks.loop(seconds=5.0)
+    async def update_status(self):
+        # update user status to show how long since last fail
+
+        time_since_last_fail = time.time() - self.last_fail
+
+        formatted_time_since_last_fail = format_elapsed_time_short(time_since_last_fail)
+
+        await self.change_presence(
+            status=discord.Status.online,
+            activity=discord.Game(f"for {formatted_time_since_last_fail}")
+        )
 
     @tasks.loop(seconds=5.0)
     async def check_if_expired(self):
+
+        
         # checks if the players missed the target time, which will reset the button to a new target time 
         if time.time() > self.target_time:
+
+            time_since_last_fail = time.time() - self.last_fail
+
+            formatted_time_since_last_fail = format_elapsed_time(time_since_last_fail)
+
+            # update last fail
+            self.last_fail = time.time()
+            
             self.set_new_target_time()
 
             # time until the next button expires
-            difference = self.target_time - time.time()
+            time_until_next_expire = self.target_time - time.time()
 
             # pretty string representing how much time until the next button needs to be pressed
-            string_difference = format_elapsed_time(difference)
+            formatted_time_until_next_expire = format_elapsed_time(time_until_next_expire)
 
             channel = await self.fetch_channel(945515760409792546)
-            await channel.send(f"You  🚨 **FAILED** 🚨  to press the button before it expired.\n\nThe new button expires in **{string_difference}**")
+            await channel.send(f"You  🚨 **FAILED** 🚨  to press the button before it expired, breaking the streak of **{formatted_time_since_last_fail}**!\n\nThe new button expires in **{formatted_time_until_next_expire}**")
+
+
+            await self.save_data()
 
     async def save_data(self):
 
         data = {
             "target_time": self.target_time,
             "high_scores": self.high_scores,
+            "last_fail": self.last_fail
         }
 
         with open(f"{self.button_data_directory}/buttondata.json", "w") as file:
